c6b7be5ee02661a0f61a21e72b6c9e2b070fc56f
Edward Lee <bert825_work@163.com>
2013-10-30 23:43:22 +0800

    策略模式
    定义了算法族，分别封装起来，让它们之间可以互相替换，此模式让算法的变化独立于使用算法的客户。（示例：模拟鸭子应用。开源框架应用：Spring
    资源访问的Resource策略接口）
    
    设计原则
        1，封装变化 —— 把会变化的部分取出并“封装”起来，好让其他部分不会受到影响。
        2，多用组合，少用继承 ——
    使用组合建立的系统具有很大的弹性，不仅可将算法族封装成类，更可以“在运行时动态地改变行为”。（“有一个”关系，好将动作“委托”别人处理。）
        3，针对接口编程，不针对实现编程 —— 利用多态，在运行时才指定具体实现的对象。（“行为”接口）

---
+++ src/main/java/com/design/pattern/strategy/Duck.java
@@ -0,0 +1,94 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.design.pattern.strategy;
+
+import com.design.pattern.strategy.action.FlyBehavior;
+import com.design.pattern.strategy.action.QuackBehavior;
+
+import static java.lang.System.out;
+
+/**
+ * 鸭子超类。
+ *
+ * @author	lihg
+ * @version 2013-10-30
+ */
+
+public abstract class Duck {
+
+	protected FlyBehavior flyBehavior;
+	protected QuackBehavior quackBehavior;
+
+	public Duck() {
+	}
+
+	/**
+	 * 展示鸭子的外观。
+	 * 
+	 * <p>
+	 * 为什么定义成抽象的？ 
+	 *    因为每一种鸭子的外观都不同。
+	 */
+	public abstract void display();
+
+
+	// 委托给行为类
+
+	/**
+	 * 展示鸭子的飞行动作。
+	 */
+	public void performFly() {
+		flyBehavior.fly();
+	}
+
+	/**
+	 * 展示鸭子的叫声。
+	 */
+	public void performQuack() {
+		quackBehavior.quack();
+	}
+
+
+	/**
+	 * 鸭子开始游泳。
+	 */
+	public void swim() {
+		out.println("All ducks float, even decoys!");
+	}
+
+
+	// 动态设定行为
+
+	/**
+	 * 动态地改变鸭子的飞行行为。
+	 *
+	 * @param flyBehavior 飞行行为
+	 */
+	public void setFlyBehavior(FlyBehavior flyBehavior) {
+		this.flyBehavior = flyBehavior;
+	}
+
+	/**
+	 * 动态地改变鸭子的叫声。
+	 *
+	 * @param quackBehavior 叫声
+	 */
+	public void setQuackBehavior(QuackBehavior quackBehavior) {
+		this.quackBehavior = quackBehavior;
+	}
+
+}
---
+++ src/main/java/com/design/pattern/strategy/RedheadDuck.java
@@ -0,0 +1,43 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.design.pattern.strategy;
+
+import com.design.pattern.strategy.action.FlyWithWings;
+import com.design.pattern.strategy.action.Quack;
+
+import static java.lang.System.out;
+
+/**
+ * 红头鸭。
+ *
+ * @author	lihg
+ * @version 2013-10-30
+ */
+
+public class RedheadDuck extends Duck {
+
+	public RedheadDuck() {
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+
+	@Override
+	public void display() {
+		out.println("I'm a real Redhead duck");
+	}
+
+}
